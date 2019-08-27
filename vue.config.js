3688d507956705adecd16ec7c37664a49a75427e
blueskyawen <bluesky.liu@163.com>
2019-08-28 01:08:59 +0800

    add publish

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   baseUrl: '/',
   outputDir: 'dist',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-demo-for-ts/' : '/',
   lintOnSave: true,
   productionSourceMap: false,
   runtimeCompiler: true,
